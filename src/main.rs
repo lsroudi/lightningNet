07b8d137f4124dec5eac71203d0d017b26557b82
Abdessamad el akroudi <lsroudi@gmail.com>
2025-11-23 12:16:44 +0100

    add test config for starting a test node

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 mod node;
 
+use node::config::{NodeConfig,NodeType};
+use std::net::SocketAddr;
+
 fn main() {
-    println!("Hello, lightning-net!");
+    println!("LightningNet is starting...");
+
+    // Test our config
+    let config = NodeConfig::new(
+        "test-node".to_string(), 
+        "127.0.0.1:8080".parse().unwrap(), 
+        NodeType::Entry);
+
+    println!("The node config created {:?}", config);    
 }
